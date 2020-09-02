84bd36fd8925a0e2885825befc3c4dce3ebfd95c
luc1dLife <luc1d_life@protonmail.com>
2020-09-02 01:13:51 -0600

    +LoadTasks +workDuration

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,39 @@ var setWorkDuration = function () {
     workStart = 6;
 };
 
+var loadTasks = function () {
+    setDate();
+    setWorkDuration();
+    var container = document.getElementById("schedule");
+    for (i = workStart; i < 19; i++) {
+        var hour = i;
+        if (i > 12) {
+            hour = hour - 12 + "PM";
+        } else if (i === 12) {
+            hour = hour + "PM";
+        } else {
+            hour = hour + "AM";
+        }
+        var task = "";
+        var html = `<div class="row mt-1">
+        <div class="col-1 time-block hour">
+            ${hour}
+        </div>
+        <div class="col-10 col-lg-10" id="s${i}">
+          <p id="day${i}" class="description">${task}</p>
+        </div>
+        <button id="saveBtn${i}" class="col-1 col-lg-1 saveBtn"><span class="fas fa-save"></span></button> 
+            
+      </div>`;
+        container.insertAdjacentHTML("beforeend", html);
+    }
+
+    $("[id^=saveBtn]").on("click", function () {
+    });
+};
+
+loadTasks();
+
 setInterval(function () {
     setDate();
 }, 1000 * 60 * 0.1);
